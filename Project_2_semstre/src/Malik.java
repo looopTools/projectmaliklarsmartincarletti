c3134c5d35caeffe996a8dc39451544e7c8df24a
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-04 10:42:07 +0000

    Added a method to Malik

--- Project_2_semstre/src/Malik.java
+++ Project_2_semstre/src/Malik.java
@@ -1,7 +1,17 @@
 
 public class Malik {
-
-	public Malik(){
-		
+	private String navn;
+	
+	public Malik(String navn){
+		this.navn = navn;
+	}
+	
+	public void printName(){
+		System.out.println(navn);
+	}
+	
+	public static void main(String[] args){
+		Malik m = new Malik("Malik");
+		m.printName();
 	}
 }
