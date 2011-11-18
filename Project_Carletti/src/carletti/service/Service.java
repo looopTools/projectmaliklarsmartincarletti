1296f14aa19e4a52736f2e46a7fbf6f64c3dcd95
martinronn@gmail.com <martinronn@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-18 09:07:10 +0000

    Service

--- Project_Carletti/src/carletti/service/Service.java
+++ Project_Carletti/src/carletti/service/Service.java
@@ -1,5 +1,41 @@
 package carletti.service;
 
-public class Service {
+import carletti.model.SubProduct;
+import carletti.model.Treatment;
+
+
+public class Service
+{
+
+	public void createSubproduct()
+	{
+		
+	}
+
+	public void deleteSubproduct(SubProduct subproduct)
+	{
+		
+	}
+	
+	public SubProduct getSubproduct(SubProduct subproduct)
+	{
+		
+	}
+	
+	public void createProduct(String name, Treatment treatment)
+	{
+		
+	}
+	
+	public void deleteProduct()
+	{
+		
+	}
+	
+	public Product getProduct()
+	{
+		
+	}
+	
 	
 }
