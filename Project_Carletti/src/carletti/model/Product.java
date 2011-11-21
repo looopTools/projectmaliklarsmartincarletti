9dba5bb0aa99774528bf3db2e0d8e1ba7481b9ab
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-21 07:39:41 +0000

    New constructor for Product

--- Project_Carletti/src/carletti/model/Product.java
+++ Project_Carletti/src/carletti/model/Product.java
@@ -15,34 +15,67 @@ public class Product {
 	private Treatment treatment;
 	
 	
-	public Product(){
-		description = new String();
+	public Product(int id, String name, String description, Treatment treatment) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.treatment = treatment;
 	}
-	
+
+	/**
+	 * Get the treatment associated with this product
+	 * type.
+	 * @return
+	 */
 	public Treatment getTreatment() {
 		return treatment;
 	}
 
+	/**
+	 * Replace the treatment associated with this product
+	 * type.
+	 * @param treatment
+	 */
 	public void setTreatment(Treatment treatment) {
 		this.treatment = treatment;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public int getId(){
 		return id;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getName(){
 		return name;
 	}
 	
+	/**
+	 * 
+	 * @param newName
+	 */
 	public void setName(String newName){
 		this.name = newName;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getDescription(){
 		return description;
 	}
 	
+	/**
+	 * 
+	 * @param newDescription
+	 */
 	public void setDescription(String newDescription){
 		this.description = newDescription;
 	}
