10984881cdc79aecea26e5239130484efc32b4c1
lnc13.lars@gmail.com <lnc13.lars@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-21 21:47:56 +0000

    Update: NewSubProduct  næsten færdig Auto gen af id: undervejs - færdig i dao og service, skal implementeres i gui dog kun for subproduct

--- Project_Carletti/src/carletti/gui/dialogs/NewSubProductDialog.java
+++ Project_Carletti/src/carletti/gui/dialogs/NewSubProductDialog.java
@@ -1,27 +1,126 @@
 package carletti.gui.dialogs;
 
 /**
- *@author Lars Nielsen, Malik Lasse Lund, Martin R¿nn Bundgaard
+ *@author Lars Nielsen, Malik Lasse Lund, Martin Rønn Bundgaard
  *@class NewSubProductDialog
  *@programmer Lars Nielsen
  */
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
+import carletti.model.Product;
+import carletti.service.Service;
+
+
+/*
+ * This class is harcoded dou to certain requriments, which windows build
+ * couldn't do.
+ */
 public class NewSubProductDialog extends JDialog{
 
-    private JTextField txfName, txfID;
+    private JTextField txfName;
+    private JLabel lblName, lblProd;
     private JScrollPane jspProducts;
     private JList prodList;
-    private int x = 20, i = 20;
-    private Dimension minimumSize = new Dimension(250, 200);
+    private JButton btnSub, btnCan; 
+    private int x = 20, y = 20;
+    private Dimension defaultSize = new Dimension(360, 360);
+    private Dimension btnSize = new Dimension(140, 20);
+    private Dimension lblSize = new Dimension(140, 20);
+    private Dimension txfSize = new Dimension(160, 20);
+    private Dimension jspSize = new Dimension(320, 160);
+
+    private Controller btnCtrl; 
+
     public NewSubProductDialog(){
-    	
+	btnCtrl = new Controller();
+	
+	this.setSize(defaultSize);
+//	this.setResizable(false);
+	this.setTitle("New Subproduct");
+	
+	lblName = new JLabel();
+	lblName.setText("Name");
+	lblName.setSize(lblSize);
+	lblName.setLocation(x,y);
+	this.add(lblName);
+
+	x += 160;
+
+	txfName = new JTextField();
+	txfName.setSize(txfSize);
+	txfName.setLocation(x,y);
+	this.add(txfName);
+
+	x = 20;
+	y += 40;
+
+	lblProd = new JLabel();
+	lblProd.setText("Products");
+	lblProd.setSize(lblSize);
+	lblProd.setLocation(x, y);
+	this.add(lblProd);
+
+	y += 40;
+
+	prodList = new JList();
+	prodList.setListData(Service.getProducts().toArray());
+	prodList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	
+	jspProducts = new JScrollPane(prodList);
+	jspProducts.setSize(jspSize);
+	jspProducts.setLocation(x,y);
+	this.add(jspProducts);
+	
+	y += 180;
+	
+	btnSub = new JButton("Submit");
+	btnSub.addActionListener(btnCtrl);
+	btnSub.setSize(btnSize);
+	btnSub.setLocation(x,y);
+	this.add(btnSub);
+	
+	x += 180;
+	
+	btnCan = new JButton("Cancel");
+	btnCan.addActionListener(btnCtrl);
+	btnCan.setSize(btnSize);
+	btnCan.setLocation(x,y);
+	this.add(btnCan);
+	
+
+	
     }
-    
+
+    private void setDialogVisibility(boolean visibility){
+	this.setVisible(visibility);
+    }
+    /**
+     * The inner class Controller, is orgnaised so we 
+     * can controll which button events is actived
+     */
+    private class Controller implements ActionListener{
+	
+	public void actionPerformed(ActionEvent ae){
+	    if(ae.getSource().equals(btnSub)){
+		//int id, String name, Product product
+		int id = Service.getNextID();
+		String name = txfName.getText();
+		Product p = (Product) prodList.getSelectedValue();
+	    }
+	    else if(ae.getSource().equals(btnCan)){
+		setDialogVisibility(false);
+	    }		
+	}
+    }
 }
