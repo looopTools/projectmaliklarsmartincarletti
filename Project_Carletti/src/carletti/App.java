37ed967ff5bd12a7a11cf55937ec0a053e2db077
lnc13.lars@gmail.com <lnc13.lars@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-21 12:05:41 +0000

    Update: show subproduct

--- Project_Carletti/src/carletti/App.java
+++ Project_Carletti/src/carletti/App.java
@@ -1,10 +1,12 @@
 package carletti;
 
 import carletti.gui.MainFrame;
+import carletti.service.Service;
 
 public class App {
 	
 	public static void main(String[] args){
+		Service.createSomeObjects();
 		MainFrame mf = new MainFrame();
 		mf.setVisible(true);
 	}
