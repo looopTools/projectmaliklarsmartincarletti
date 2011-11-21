af5021bce6114d0d809010256213c24eb3fe6eaa
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-21 10:25:07 +0000

    * Added a object creation method to Service (so that we have some objects to play around with). * Changed the constructor in SubProduct, so that it no longer accepts a State as a parameter.

--- Project_Carletti/src/carletti/service/Service.java
+++ Project_Carletti/src/carletti/service/Service.java
@@ -21,10 +21,11 @@ public class Service
 	
 	private Dao dao;
 	
-	public void createSubProduct(int id, String name, State state, Product product)
+	public SubProduct createSubProduct(int id, String name, Product product)
 	{
-		SubProduct sp = new SubProduct(id, name, state, product);
+		SubProduct sp = new SubProduct(id, name, product);
 		dao.storeSubProduct(sp);
+		return sp;
 	}
 	
 	public void removeSubproduct(SubProduct subProduct)
@@ -44,11 +45,17 @@ public class Service
 		return dao.getSubProducts().get(i);
 	}
 	
-	public void createProduct(int id, String name, String description, Treatment treatment)
+	public Treatment createTreatment(String name)
 	{
-//		Constructor missing in product!
+		Treatment t = new Treatment(name);
+		return t;
+	}
+	
+	public Product createProduct(int id, String name, String description, Treatment treatment)
+	{
 		Product p = new Product(id, name, description, treatment);
 		dao.storeProduct(p);
+		return p;
 	}
 	
 	public void removeProduct(Product product)
@@ -89,5 +96,32 @@ public class Service
 	{
 		int i = dao.getSubProducts().indexOf(subProduct);
 		return dao.getSubProducts().get(i) + "";
-	}	
+	}
+	
+	/**
+	 * 
+	 */
+	public void createSomeObjects()
+	{
+		Treatment t1 = createTreatment("Red chocolate MMs");
+		t1.createSubTreatment("1st drying", 1000, 1250, 1500);
+		t1.createSubTreatment("2nd drying", 500, 750, 1000);
+		t1.createSubTreatment("3rd drying", 1250, 1300, 1500);
+		Product p1 = createProduct(1, "Red Chocolate MMs", "Info about red chocolate MMs", t1);
+		
+		Treatment t2 = createTreatment("Liquorice");
+		t2.createSubTreatment("1st drying", 1000, 1250, 1500);
+		t2.createSubTreatment("2nd drying", 1500, 1750, 2000);
+		Product p2 = createProduct(2, "Liquorice", "Liquorice with coloured sugar layer", t2);
+		
+		Treatment t3 = createTreatment("Coffeebean");
+		t3.createSubTreatment("1st drying", 500, 600, 700);
+		t3.createSubTreatment("2nd drying", 1200, 1300, 1400);
+		t3.createSubTreatment("3rd drying", 300, 400, 500);
+		Product p3 = createProduct(3, "Coffee Bean", "Coffee paste with a layer of chocolate", t3);
+		
+		SubProduct sp1 = createSubProduct(1, "Foobar", p1);
+		SubProduct sp2 = createSubProduct(2, "Barbaz", p2);
+		SubProduct sp3 = createSubProduct(3, "Bazfoo", p3);
+	}
 }
