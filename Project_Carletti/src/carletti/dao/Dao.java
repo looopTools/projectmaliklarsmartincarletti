0429a213e1657fdf21801f3934f89f55a6a90439
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-11-29 10:02:04 +0000

    Added methods to Dao, so that you can save, remove and get Treatment-objects seperately from Products.

--- Project_Carletti/src/carletti/dao/Dao.java
+++ Project_Carletti/src/carletti/dao/Dao.java
@@ -5,19 +5,19 @@ import java.util.List;
 import carletti.model.Product;
 import carletti.model.State;
 import carletti.model.SubProduct;
+import carletti.model.Treatment;
 
 public interface Dao {
 
 	public List<SubProduct> getSubProducts(State state);
 	public SubProduct changeStateOfSubProduct(SubProduct subProduct, State state);
+	public void storeTreatment(Treatment t);
+	public void removeTreatment(Treatment t);
 	public void storeProduct(Product p);
 	public void removeProduct(Product p);
 	public void storeSubProduct(SubProduct sp);
 	public void removeSubProduct(SubProduct sp);
+	public List<Treatment> getTreatments();
 	public List<Product> getProducts();
 	public List<SubProduct> getSubProducts();
-	public int getSubProducNextID();
-	public void countSubProducID();
-	public int getProducNextID();
-	public void countProducID();
 }
