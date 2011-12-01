b074d37f2d039f726e831a2a9cd2c2540c51635e
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-12-01 10:50:29 +0000

    Dao tests and minor changes to createNewProductDialog

--- Project_Carletti/src/carletti/dao/DaoTest.java
+++ Project_Carletti/src/carletti/dao/DaoTest.java
@@ -8,6 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import carletti.model.Position;
 import carletti.model.Product;
 import carletti.model.SubProduct;
 import carletti.model.Treatment;
@@ -22,14 +23,12 @@ public class DaoTest {
 
 	@Before
 	public void setUp() throws Exception {
-		dao = null;
-		Thread.sleep(100);
-		dao = JpaDao.getInstance();
+		
 	}
 	
 	@After
 	/**
-	 * 
+	 * Delete everything before starting new test.
 	 */
 	public void cleanUp(){
 		for (SubProduct sp: dao.getSubProducts()){
@@ -52,9 +51,72 @@ public class DaoTest {
 		t1.createSubTreatment("st2", 1000, 2000, 3000);
 		dao.storeTreatment(t1);
 		List<Treatment> treatments = dao.getTreatments();
+		
 		assertEquals("Size differs from 1", 1, treatments.size());
+		
 		assertEquals("Name differs", "TestTreatment", treatments.get(0).getName());
+		
 		Treatment retrievedTreatment = treatments.get(0);
+		
 		assertEquals("1st SubTreatmentDiffers", t1.getSubTreatments().get(0), retrievedTreatment.getSubTreatments().get(0));
+	}
+	
+	@Test
+	public void removeTreatmentTest(){
+		Treatment t1 = new Treatment("TestTreatment");
+		t1.createSubTreatment("st1", 1000, 2000, 3000);
+		t1.createSubTreatment("st2", 1000, 2000, 3000);
+		dao.storeTreatment(t1);
+		dao.removeTreatment(t1);
+		
+		assertEquals("List of treatments not empty", 0, dao.getTreatments().size());
+	}
+	
+	@Test
+	public void storeProductTest(){
+		Treatment t1 = new Treatment("TestTreatment");
+		t1.createSubTreatment("st1", 1000, 2000, 3000);
+		t1.createSubTreatment("st2", 1000, 2000, 3000);
+		dao.storeTreatment(t1);
+		Product p1 = new Product("Product1", "ProductDescription", t1);
+		dao.storeProduct(p1);
+		List<Product> products = dao.getProducts();
+		
+		assertEquals("Size is not 1", 1, products.size());
+		
+		Product retrievedProduct = dao.getProducts().get(0);
+		
+		assertEquals("Products differs", p1, retrievedProduct);
+		
+		assertEquals("Treatment differs", t1, retrievedProduct.getTreatment());
+	}
+	
+	@Test
+	public void removeProductTest(){
+		Treatment t1 = new Treatment("TestTreatment");
+		t1.createSubTreatment("st1", 1000, 2000, 3000);
+		t1.createSubTreatment("st2", 1000, 2000, 3000);
+		Product p1 = new Product("Product1", "ProductDescription", t1);
+		dao.storeProduct(p1);
+		dao.removeProduct(p1);
+		
+		assertEquals("Product not deleted", 0, dao.getProducts().size());
+	}
+	
+	@Test
+	public void storeSubProductTest(){
+		Treatment t1 = new Treatment("TestTreatment");
+		t1.createSubTreatment("st1", 1000, 2000, 3000);
+		t1.createSubTreatment("st2", 1000, 2000, 3000);
+		Product p1 = new Product("Product1", "ProductDescription", t1);
+		dao.storeProduct(p1);
+		SubProduct sp = new SubProduct("sp1", dao.getProducts().get(0), System.currentTimeMillis(), new Position("2A"));
+		dao.storeSubProduct(sp);
+		
+		assertEquals("List of subproducts differs from 1", 1, dao.getProducts().size());
+		
+		assertEquals("Subproducts differ", sp, dao.getSubProducts().get(0));
+		
+		assertEquals("Product differs", p1.getName(), dao.getProducts().get(0).getName());
 	}
 }
