a79b50fa74b95e258ac5484d1c595ee1bbfb7d73
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-12-09 08:51:45 +0000

    DatabaseConnectTester

--- Project_Carletti_CAOS/src/carletti/DatabaseConnectTester.java
+++ Project_Carletti_CAOS/src/carletti/DatabaseConnectTester.java
@@ -2,7 +2,10 @@ package carletti;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnectTester {
 	
@@ -18,6 +21,26 @@ public class DatabaseConnectTester {
 					"jdbc:jtds:sqlserver://Malik-PB/Carletti",
 					"sa",
 					"sa");
+			Statement stmt = myConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet res = stmt.executeQuery("SELECT * FROM SUBPRODUCT;");
+			ResultSetMetaData resMeta = res.getMetaData();
+			while (res.next()){
+				StringBuffer buffer = new StringBuffer();
+				for (int i = 1; i < resMeta.getColumnCount()+1; i++){
+					buffer.append(resMeta.getColumnName(i) + ": " +  res.getString(i) + " ");
+				}
+				System.out.println(buffer);
+			}
+			
+			if (res != null){
+				res.close();
+			}
+			if (stmt != null){
+				stmt.close();
+			}
+			if (myConnection != null){
+				myConnection.close();
+			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
