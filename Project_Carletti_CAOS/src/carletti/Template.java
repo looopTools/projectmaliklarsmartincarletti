987d6bf7377788fc0c7b27fcb2e2e4e93df6c766
lnc13.lars@gmail.com <lnc13.lars@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-12-09 08:26:41 +0000

    A template classed which should be used as templated

---
+++ Project_Carletti_CAOS/src/carletti/Template.java
@@ -0,0 +1,24 @@
+//Package declearation
+package carletti;
+
+
+//Imports
+
+
+/**
+ * 
+ * @group Martin R. Bundgaard, Malik L. Lund, Lars Nielsen
+ * @author Lars Nielsen
+ * @class
+ *
+ */
+
+
+public class Template {
+
+	//Fields
+	
+	//Constructor
+	
+	//Methods
+}
