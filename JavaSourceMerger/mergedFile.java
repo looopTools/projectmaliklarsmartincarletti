e904b7fbeb65b5e51d9b0a75305a3261ecefed1f
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-12-12 01:51:15 +0000

    A very small and simple program that merges every .java file in every directory, and subdirectory, into one big file.

---
+++ JavaSourceMerger/mergedFile.java
@@ -0,0 +1,60 @@
+
+---- D:\Workspace\Java\JavaSourceMerger\.\mergedFile.java
+
+
+---- D:\Workspace\Java\JavaSourceMerger\.\src\maliklund87\javasourcemerger\SourceMerger.java
+
+package maliklund87.javasourcemerger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Goes through every subfolder in the current directory,
+ * finds all the .java files and merges them into a single
+ * file.
+ * @author Malik Lund
+ *
+ */
+public class SourceMerger {
+	private static FileWriter writer;
+	
+	public static void merge(String path) throws IOException{
+		File output = new File("mergedFile.java");
+		writer = new FileWriter(output);
+		handleDirectory(new File(path));
+		writer.close();
+	}
+	
+	private static void handleDirectory(File file) throws IOException{
+		if (file.isDirectory()){
+			File[] fileList = file.listFiles();
+			for (File f: fileList){
+				handleDirectory(f);
+			}
+		} else {
+			if (file.getAbsolutePath().endsWith(".java")){
+				handleFile(file);
+			}
+		}
+	}
+	
+	private static void handleFile(File file) throws IOException{
+		Scanner scanner = new Scanner(file);
+		writer.write("\n---- " + file.getAbsolutePath() + "\n\n");
+		while (scanner.hasNext()){
+			writer.write(scanner.nextLine() + "\n");
+		}
+		scanner.close();
+	}
+	
+	public static void main(String[] args){
+		try {
+			SourceMerger.merge(".");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
