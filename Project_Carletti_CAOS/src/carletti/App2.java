2beb38359d28981f84f13008ea9f1ceb9202d226
maliklund87@gmail.com <maliklund87@gmail.com@dd1afad3-0349-c845-6d0d-bdf380631883>
2011-12-14 20:21:09 +0000

    Changed connection to database to happen in one place, rather than in each class.

--- Project_Carletti_CAOS/src/carletti/App2.java
+++ Project_Carletti_CAOS/src/carletti/App2.java
@@ -1,5 +1,8 @@
 package carletti;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,10 +10,18 @@ public class App2 {
 
 	/**
 	 * @param args
+	 * @throws ClassNotFoundException 
+	 * @throws SQLException 
 	 */
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("net.sourceforge.jtds.jdbc.Driver");
+		String url = args[1];
+		String username = args[2];
+		String password = args[3];
+		Connection connection = DriverManager.getConnection(url, username, password);
+		
 		String[] commands = {
 			" a: nextTreatment",
 			" b: printSubProducts",
@@ -29,9 +40,9 @@ public class App2 {
 		
 		HashMap<String, Runnable> runnables = new HashMap<String, Runnable>();
 		runnables.put("a", new Opgave2PunktA()); //Lars 
-		runnables.put("b", new Opgave2PunktB());
-		runnables.put("d", new Opgave2PunktD());
-		runnables.put("g", new Opgave2PunktG());
+		runnables.put("b", new Opgave2PunktB(connection));
+		runnables.put("d", new Opgave2PunktD(connection));
+		runnables.put("g", new Opgave2PunktG(connection));
 		runnables.put("c", new Opgave2PunktC());
 		runnables.put("e", new Opgave2PunktE());
 		
